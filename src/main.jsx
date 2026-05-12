bb496539aeb37531a5bcb17a8eca55fbc97860ae
Miguel10 <miguelangeluruetaballesteros@gmail.com>
2026-05-12 14:41:28 -0500

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import './index.css'
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
       <App />
+      <Analytics />
     </CartProvider>
   </React.StrictMode>,
 )
